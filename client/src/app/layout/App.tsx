522e2425754d06f695469990436d25476e2cfbd2
Wend-Kuuni <belemgnegreetienne@gmail.com>
2024-06-30 23:48:08 +0000

    added darkMode

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,12 +1,33 @@
 
-import './App.css'
+// import './App.css'
+import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 import Catalog from '../../features/Catalog'
+import Header from './Header'
+import { useState } from 'react'
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+    }
+  })
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
 
   return (
     <>
-      <Catalog />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+        <Container sx={{ paddingTop: "84px" }}>
+          <Catalog />
+        </Container>
+      </ThemeProvider >
     </>
   )
 }
